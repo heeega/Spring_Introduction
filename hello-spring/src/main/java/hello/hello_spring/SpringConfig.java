32b950661dadbb9e3b560434de5df2f8f5f560d3
heega <m_mny1004@naver.com>
2024-05-28 18:24:31 +0900

    sec06

--- hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,21 +1,42 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+
+    // DB 변경시 return 값만 변경해주면 됨
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+
+//    }
+
+
+
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
+
     }
+
 }
